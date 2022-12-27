c0fa0aef3dcab79207264d26db1317992da4ab99
baesuhan <105197524+uh004@users.noreply.github.com>
2022-12-27 21:01:46 +0900

    Create test2.py

---
+++ lv0/test2.py
@@ -0,0 +1,13 @@
+def solution(array, height):
+    answer = 0
+    for i in array:
+        if i > height:
+            answer += 1
+    return answer
+
+def solution(array, height):
+    answer = 0
+    for i in range(len(array)):
+        if array[i] > height:
+            answer += 1
+    return answer
