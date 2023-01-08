f83f0e6b123134d6efbc006aaa581f392dd1797f
baesuhan <105197524+uh004@users.noreply.github.com>
2023-01-08 23:39:51 +0900

    Update and rename test7.py to 두 수의 나눗셈.py

--- lv0/test7.py
+++
@@ -1,5 +0,0 @@
-# 두 수의 나눗셈
-def solution(num1, num2):
-    answer = 0
-    answer = round((num1 / num2) * 1000)
-    return answer
---
+++ lv0/두 수의 나눗셈.py
@@ -0,0 +1,8 @@
+# 두 수의 나눗셈
+def solution(num1, num2):
+    answer = 0
+    answer = int(num1 / num2 * 1000)
+    return answer
+def solution(num1, num2):
+    answer = 0
+    return 1000*num1//num2
