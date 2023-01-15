b974949962d9f188d172937f3f036f35c0a6feac
baesuhan <105197524+uh004@users.noreply.github.com>
2023-01-15 20:20:17 +0900

    Create 짝수의 합.py

---
+++ lv0/짝수의 합.py
@@ -0,0 +1,11 @@
+# 짝수의 합
+
+def solution(n):
+    return sum([i for i in range(2, n + 1, 2)])
+
+def solution(n):
+    answer = 0
+    for i in range(n+1):
+        if i % 2 == 0:
+            answer += i
+    return answer
