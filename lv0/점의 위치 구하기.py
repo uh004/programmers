3f585ee4b6090fc9030cc4a8ed1340cc8c69ba7d
baesuhan <105197524+uh004@users.noreply.github.com>
2023-01-16 19:16:24 +0900

    Create 점의 위치 구하기.py

---
+++ lv0/점의 위치 구하기.py
@@ -0,0 +1,11 @@
+# 점의 위치 구하기
+
+def solution(dot):
+    if dot[0] > 0 and dot[1] > 0:
+        return 1
+    elif dot[0] < 0 and dot[1] > 0:
+        return 2
+    elif dot[0] < 0 and dot[1] < 0:
+        return 3
+    elif dot[0] > 0 and dot[1] < 0:
+        return 4
