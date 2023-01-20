ab10c3faf8aeabd9aeabfdb41d0d60afd3cd1b17
baesuhan <105197524+uh004@users.noreply.github.com>
2023-01-21 00:12:05 +0900

    Create 문자열안에 문자열.py

---
+++ lv0/문자열안에 문자열.py
@@ -0,0 +1,11 @@
+# 문자열안에 문자열
+
+def solution(str1, str2):
+    return 1 if str2 in str1 else 2
+
+def solution(str1, str2):
+    
+    if str2 in str1:
+        return 1
+    else:
+        return 2
