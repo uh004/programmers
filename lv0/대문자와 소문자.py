4998f1200c667832448ea9e0a7004764feae0e11
baesuhan <105197524+uh004@users.noreply.github.com>
2023-01-21 12:49:46 +0900

    Create 대문자와 소문자.py

---
+++ lv0/대문자와 소문자.py
@@ -0,0 +1,10 @@
+# 대문자와 소문자
+
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i.isupper():
+            answer += i.lower()
+        else:
+            answer += i.upper()
+    return answer
