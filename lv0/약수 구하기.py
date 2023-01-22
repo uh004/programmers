fcb9572a630fd09cc89e033379dbef7913d015e5
baesuhan <105197524+uh004@users.noreply.github.com>
2023-01-22 19:49:55 +0900

    Create 약수 구하기.py

---
+++ lv0/약수 구하기.py
@@ -0,0 +1,13 @@
+# 약수 구하기
+
+def solution(n):
+    answer = [i for i in range(1,n+1) if n%i == 0]
+    return answer
+
+def solution(n):
+    answer = []
+    for i in range(1,n+1):
+        if n % i == 0:
+            answer.append(i)
+    
+    return answer
