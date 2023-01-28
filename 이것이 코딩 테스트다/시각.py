6a21ed8486bb93660128d02f4ab4e22321c76e4f
baesuhan <105197524+uh004@users.noreply.github.com>
2023-01-29 00:18:16 +0900

    Create 시각.py

---
+++ 이것이 코딩 테스트다/시각.py
@@ -0,0 +1,14 @@
+# 시각 
+
+# H 입력 받기
+h = int(input())
+
+count = 0
+for i in range(h+1): # 시
+    for j in range(60): # 분
+        for k in range(60): # 초
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+                
+print(count)
