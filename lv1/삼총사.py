5759a4e90bda2e991220f111ede62949851b0fa0
baesuhan <105197524+uh004@users.noreply.github.com>
2023-03-21 22:01:43 +0900

    Create 삼총사.py

---
+++ lv1/삼총사.py
@@ -0,0 +1,16 @@
+def solution(number):
+    answer = 0
+    for i in range(len(number)-2):
+        for j in range(i+1, len(number)-1):
+            for k in range(j+1, len(number)):
+                if number[i]+number[j]+number[k] == 0:
+                    answer += 1
+    return answer
+  
+def solution (number) :
+    from itertools import combinations
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
+    return cnt
